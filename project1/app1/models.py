586bdca154f0c04467b9690d4709c0104f55de8e
al24057 <yushitennis3@icloud.com>
2025-08-12 16:51:32 +0900

    a

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -22,7 +22,7 @@ class  Article(models.Model):
         return self.title
     
 class Tags(models.Model):
-    name=models.CharField(max_length=41,unique=True)
+    name=models.CharField(max_length=42,unique=True)
     
     def __str__(self):
         return self.name
